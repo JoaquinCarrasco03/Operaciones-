02871ea6761512d82142a1925012fb6a26f2a7bb
JoaquinCarrasco03 <A20150104@juan23.edu.pe>
2024-11-04 13:28:01 -0500

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,27 @@
+document.write("<h1> Operaciones Bascias</h1>");
+
+var num1 = parseInt(prompt("Ingresa primer número"));
+var num2 = parseInt(prompt("Ingresa segundo número"));
+w;
+
+var sum = num1 + num2;
+var res = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+
+document.write("El primer número fue" + num1);
+document.write("<br>");
+document.write("El segundo número fue" + num2);
+document.write("<br>");
+document.write("La suma de ambos es:", +sum);
+document.write("<br>");
+document.write("La resta de ambos es:", +res);
+document.write("<br>");
+document.write("El producto de ambos es:", +mul);
+document.write("<br>");
+document.write("La divicion de ambos es:", +div);
+document.write("<br>");
+document.write("<br>");
+document.write(
+  "<marquee><h2>Hecho por Joaquin Carrasco Duffaut</h2></marquee>"
+);
